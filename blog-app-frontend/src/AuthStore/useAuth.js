b1e0b3540335ea6d53b289268b4bf22468147a3b
nithin <nithin@gmail.com>
2026-04-18 21:05:20 +0530

    frontend change

--- blog-app-frontend/src/AuthStore/useAuth.js
+++ blog-app-frontend/src/AuthStore/useAuth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { persist } from "zustand/middleware"; // 1. Import persist middleware
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://blog-app-bivn.onrender.com";
 export const useAuth = create(
   persist(
     (set) => ({
